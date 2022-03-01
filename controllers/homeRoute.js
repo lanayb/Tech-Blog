359f33adabc9031dae6d43f8f8ebcf10e7b72e9e
Lanay Brown <lanayb.03@gmail.com>
2022-03-01 11:04:47 -0500

    fixed home routes

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -3,21 +3,24 @@ const { User, Post } = require("../models");
 const path = require('path');
 
 
-router.get('/home', async(req, res) => {
-    try {
-        res.render('homePage', {});
-    } catch(err) {
-        res.status(400).json(err);
-    }
+router.get('/', async(req, res) => {
+    const userData = await User.findAll().catch((err) => {
+        res.json(err);
+    });
+    const home = userData.map((home) => home.get({ plain:true }));
+    res.render('home', {home});
 });
 
-router.get('/signup', async(req, res) => {
-    try{
-        res.render('signUp', {});
-    } catch(err) {
-        res.status(400).json(err);
-    }
-});
+
+// router.get('/signup', async(req, res) => {
+//     const signupPg = await User.findAll().catch((err) => {
+//          res.json(err);
+//     });
+//     const signup = signupPg.map((signup) => signup.get({ plain:true }));
+//     res.render('signup', {signup});
+//  });
+
+
 
 router.get('/login', async(req, res) => {
     try{
